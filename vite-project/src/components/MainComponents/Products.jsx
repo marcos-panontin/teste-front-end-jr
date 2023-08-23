a95cccfb12e3fc46ebcb1dcdc772eb98388d2160
Marcos Panontin <amarcospanontin@gmail.com>
2023-08-23 19:28:38 -0300

    feat: implements Carousel Layout, without animation

--- vite-project/src/components/MainComponents/Products.jsx
+++ vite-project/src/components/MainComponents/Products.jsx
@@ -2,24 +2,41 @@ import { useState, useEffect } from 'react';
 import { fetchProducts } from '../../services/api';
 import ProductCard from './ProductCard';
 
-
 export default function Products() {
-    useEffect(() => {
-    async function fetchData() {
-      const products = await fetchProducts();
-      setProducts(products)
-    }
-      fetchData();     
+  const [products, setProducts] = useState([]);
+  const [indexLimit, setIndexLimit] = useState(0);
 
-    }, [])
-  
-  const [products, setProducts] = useState([])
-  
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedProducts = await fetchProducts();
+      setProducts(fetchedProducts);
+    }
+    fetchData();
+  }, []);
+
+  const productCount = products.length;
+  const itemsToShow = 4;
+
+  const handlePrevClick = () => {
+    setIndexLimit((indexLimit - 1 + productCount) % productCount);
+  };
+
+  const handleNextClick = () => {
+    setIndexLimit((indexLimit + 1) % productCount);
+  };
+
+  const visibleProducts = products.slice(indexLimit, indexLimit + itemsToShow);
+  if (visibleProducts.length < itemsToShow) {
+    visibleProducts.push(...products.slice(0, itemsToShow - visibleProducts.length));
+  }
+
   return (
-    <section className='main__products'>
-      {products.filter((product, index) => index < 4).map((product, index) => (
+    <section className='main__products carousel-animation' style={{ transform: `translateX(270px)` }}>
+      <button onClick={handlePrevClick}>PREV</button>
+      {visibleProducts.map((product, index) => (
         <ProductCard key={`${product}${index}`} product={product} />
       ))}
+      <button onClick={handleNextClick}>NEXT</button>
     </section>
-  )
+  );
 }
